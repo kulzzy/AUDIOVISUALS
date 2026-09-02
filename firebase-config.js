e99b74f3c2a5c2c96a3174e31f8bbbdd5198808f
Chukwuma Peter Chinedu <donshangti2@gmail.com>
2026-09-02 16:12:26 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// =====================================================
+// KULZZY RADIO AUDIOVISUALS
+// FIREBASE CONFIGURATION
+// =====================================================
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCheGUZWbyh3IS4l_MbToCyBgYDre8ZU_M",
+  authDomain: "audiovisuals-500cf.firebaseapp.com",
+  databaseURL: "https://audiovisuals-500cf-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "audiovisuals-500cf",
+  storageBucket: "audiovisuals-500cf.firebasestorage.app",
+  messagingSenderId: "34375747661",
+  appId: "1:34375747661:web:94ec087328dc5daebd3404",
+  measurementId: "G-HE48JBFLD6"
+};
